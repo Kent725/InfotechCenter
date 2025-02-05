acd127847f0b18dae62f7a0bb46552b1c52d528e
Kent7763 <teriyakikent725@gmail.com>
2025-02-05 14:16:02 -0500

    Added Weather() - Stable

--- BlackWidow.py
+++ BlackWidow.py
@@ -1,4 +1,15 @@
-# Programmer: Kent Chheu
-# Branch: Weather
-# Date:
-# Progress
+print("\n*****************************************\n")
+
+print("Weather Branch - Developer: Kent Chheu\n")
+
+#Import Libraries HERE
+import random
+from time import sleep
+
+# Weather Function to Determine the Weather
+def weather():
+    weatherForecastList = ["snowing", "blizzard", "icy", "rainy", "windy", "sunny"]
+    weatherCondition = random.choice(weatherForecastList)
+    return weatherCondition
+
+print(weather())
